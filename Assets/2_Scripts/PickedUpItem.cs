e94ea5bdb4822a58c4bfad8aef7b19af705cdf16
Kei-eu <kevin.cardoen@gmail.com>
2020-01-14 15:47:16 +0100

    PickUpItems

---
+++ Assets/2_Scripts/PickedUpItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickedUpItem : MonoBehaviour
+{
+	//Collect Ressources
+	private void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.tag == "Player")
+		{
+			print(gameObject.name + " is picked up!");
+			//Variable Lightbulb ++
+			Destroy(gameObject);
+
+		}
+	}
+
+}

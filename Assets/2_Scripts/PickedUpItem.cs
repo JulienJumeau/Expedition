5c1ab612436ea66b0b169d9cd7bb7976ed0a2bee
Kei-eu <kevin.cardoen@gmail.com>
2020-01-15 15:29:47 +0100

    Unrevert "PickUpObjects modif"
    This reverts commit eb1c27c72b230c2957400c9de9b1532059e3fcf7.

--- Assets/2_Scripts/PickedUpItem.cs
+++ Assets/2_Scripts/PickedUpItem.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class PickedUpItem : MonoBehaviour
 {
-	public static int LightbulbNumber;
-	[Range (1f, 5f)]
-	public int LightbulbNumberMax = 2;
-	public int OilLevel; //Between 0 and 1
-
-	private void Start()
-	{
-		LightbulbNumber = 0;
-		OilLevel = 0;
-	}
+	//Collect Ressources
 	private void OnTriggerEnter(Collider collider)
 	{
-		if (collider.gameObject.tag == "Player")
+		if(collider.gameObject.tag == "Player")
 		{
-			if (gameObject.tag == "Lightbulb")
-			{
-				if (LightbulbNumber < LightbulbNumberMax)
-				{
-					LightbulbNumber++;
-					print("New Lightbulb picked up! You now have [" + LightbulbNumber + "] Lightbulbs in your inventory.");
-					Destroy(gameObject);
-				}
-				else print("You reach the maximum number of Lightbulbs that you can carry in your inventory.");
-			}
-			if (gameObject.tag == "Oil")
-			{
-				OilLevel = 1;
-				print("Your oil level is now full!");
-				Destroy(gameObject);
-			}
+			print(gameObject.name + " is picked up!");
+			//Variable Lightbulb ++
+			Destroy(gameObject);
+
 		}
 	}
 

669b033bbc1aa0f44a18529a84e30663e3626b30
Jumeau Julien <57345312+JulienJumeau@users.noreply.github.com>
2020-02-05 17:34:46 +0100

    Running Pingu Script + Place in LDTuto ( Need to be tweaked by GD )

---
+++ Assets/2_Scripts/Foe/PenguinRunner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PenguinRunner : MonoBehaviour
+{
+	#region Variables declaration
+
+	[SerializeField] Transform[] _destinatinationPoints = null;
+	private NavMeshAgent _agent;
+	private Animator _animator;
+
+	#endregion
+
+	#region Unity Methods
+
+	private void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+		_animator = GetComponentInChildren<Animator>();
+		this.transform.position = _destinatinationPoints[0].position;
+	}
+
+	private void Start()
+	{
+		_animator.SetBool("P_IsRunning", true);
+		_agent.SetDestination(_destinatinationPoints[1].position);
+	}
+
+	private void Update()
+	{
+		if (_agent.remainingDistance < 0.1f)
+		{
+			Destroy(this.transform.parent.gameObject);
+		}
+	}
+
+	#endregion
+}

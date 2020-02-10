b65a12fa0d4bca3533720d6b1dd27b9892f59aa2
Kei-eu <kevin.cardoen@gmail.com>
2020-02-10 10:33:45 +0100

    Sound class + SonManager + Folder for scripts

---
+++ Assets/2_Scripts/Sounds/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+	public string _name;
+	public AudioClip _audioClip;
+
+	[Range(0f, 1f)]
+	public float _volume;
+
+	public bool loop;
+
+	[HideInInspector]
+	public AudioSource _audioSource;
+
+}

50e7a7ad7ba0e1f30f77408507ef92cfb2c8dd52
Jumeau Julien <57345312+JulienJumeau@users.noreply.github.com>
2020-02-13 15:15:36 +0100

    Fix Player Animation during Reading + Event for fuiture pingu sound

---
+++ Assets/2_Scripts/Foe/PenguinAnimationEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class PenguinAnimationEvent : MonoBehaviour
+{
+    public void Hit()
+    {
+        Debug.Log("Hit");
+    }
+
+    #region Events
+
+    public event EventHandler OnFootStepRun;
+
+    private void FootStepRun() => OnFootStepRun?.Invoke(this, EventArgs.Empty);
+
+    #endregion
+
+}

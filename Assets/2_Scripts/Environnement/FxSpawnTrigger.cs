d98a85cf7d97c99ea1b01ce295087b8e333a72f6
Kei-eu <kevin.cardoen@gmail.com>
2020-02-13 14:22:48 +0100

    Fx Trigger Area

---
+++ Assets/2_Scripts/Environnement/FxSpawnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxSpawnTrigger : MonoBehaviour
+{
+	#region Variables declaration
+
+	[SerializeField] private GameObject _fxToSpawn = null;
+	private BoxCollider _collider;
+
+	#endregion
+
+	#region Unity Methods
+
+	private void Awake()
+	{
+		_collider = GetComponent<BoxCollider>();
+	}
+
+	private void OnTriggerEnter(Collider _)
+	{
+		Debug.Log("Triggered");
+		_fxToSpawn.SetActive(true);
+		_collider.enabled = false;
+	}
+
+	#endregion
+}
